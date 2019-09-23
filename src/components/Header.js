d09cd32bc47f738fe0dc472c63c636e7885012ac
Andy Tran <andy8tran@gmail.com>
2019-09-23 17:05:38 -0400

    class -> classname

--- src/components/Header.js
+++ src/components/Header.js
@@ -65,7 +65,7 @@ const Header = ({ location }) => (
               }}
             >
               <span className={'sr-only'}>Toggle navigation</span>
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
           </header>
 
