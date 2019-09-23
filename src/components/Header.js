e21f18be3de2b9871b2b168446e2e26d3ae60700
Andy Tran <andy8tran@gmail.com>
2019-09-23 10:12:45 -0400

    hide links in header/sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-import GitHubButton from 'react-github-btn'
+import GitHubButton from 'react-github-btn';
 import Link from './link';
 import './styles.css';
 import config from '../../config.js';
 
 import Search from './search/index';
 const help = require('./images/help.svg');
-const isSearchEnabled = config.header.search && config.header.search.enabled ? true : false;
+const isSearchEnabled =
+  config.header.search && config.header.search.enabled ? true : false;
 
 let searchIndices = [];
-if(isSearchEnabled && config.header.search.indexName) {
-  searchIndices.push(
-    { name: `${config.header.search.indexName}`, title: `Results`, hitComp: `PageHit` },
-  );
+if (isSearchEnabled && config.header.search.indexName) {
+  searchIndices.push({
+    name: `${config.header.search.indexName}`,
+    title: `Results`,
+    hitComp: `PageHit`,
+  });
 }
 
-import Sidebar from "./sidebar";
+import Sidebar from './sidebar';
 
-const Header = ({location}) => (
+const Header = ({ location }) => (
   <StaticQuery
-    query={
-      graphql`
-        query headerTitleQuery {
-          site {
-            siteMetadata {
-              headerTitle
-              githubUrl
-              helpUrl
-              tweetText
-              logo {
-                link
-                image
-              }
-              headerLinks {
-                link
-                text
-              }
+    query={graphql`
+      query headerTitleQuery {
+        site {
+          siteMetadata {
+            headerTitle
+            githubUrl
+            helpUrl
+            tweetText
+            logo {
+              link
+              image
+            }
+            headerLinks {
+              link
+              text
             }
           }
         }
-        `}
-    render={(data) => {
-      const logoImg = require('./images/logo.svg');
+      }
+    `}
+    render={data => {
+      const logoImg = require('./images/km_logo.svg');
       const twitter = require('./images/twitter.svg');
       const {
         site: {
@@ -53,45 +55,51 @@ const Header = ({location}) => (
             tweetText,
             logo,
             headerLinks,
-          }
-        }
+          },
+        },
       } = data;
       const finalLogoLink = logo.link !== '' ? logo.link : '/';
       return (
         <div className={'navBarWrapper'}>
           <nav className={'navbar navbar-default navBarDefault'}>
-            <div className={'navbar-header navBarHeader'}>
+            <header className={'navbar-header navBarHeader'}>
               <Link to={finalLogoLink} className={'navbar-brand navBarBrand'}>
-                {logo.image !== '' ?
-                  (<img className={'img-responsive'} src={logo.image} alt={'logo'} />)
-                  :
-                  (<img className={'img-responsive'} src={logoImg} alt={'logo'} />)
-                }
-                <div className={"headerTitle"} dangerouslySetInnerHTML={{__html: headerTitle}} />
+                <img className={'img-responsive'} src={logoImg} alt={'logo'} />
+                <span className={'headerTitle'}>{headerTitle}</span>
               </Link>
-              <button type="button" className={'navbar-toggle collapsed navBarToggle'} data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <button
+                type="button"
+                className={'navbar-toggle collapsed navBarToggle'}
+                data-toggle="collapse"
+                data-target="#navbar"
+                aria-expanded="false"
+                aria-controls="navbar"
+              >
                 <span className={'sr-only'}>Toggle navigation</span>
                 <span className={'icon-bar'}></span>
                 <span className={'icon-bar'}></span>
                 <span className={'icon-bar'}></span>
               </button>
-            </div>
-            {isSearchEnabled ? (
+            </header>
+            {/* {isSearchEnabled ? (
               <div className={'searchWrapper hidden-xs navBarUL'}>
                 <Search collapse indices={searchIndices} />
               </div>
-              ): null}
-            <div id="navbar" className={'navbar-collapse collapse navBarCollapse'}>
+            ) : null} */}
+            <div
+              id="navbar"
+              className={'navbar-collapse collapse navBarCollapse'}
+            >
               <div className={'visible-xs'}>
                 <Sidebar location={location} />
-                <hr/>
+                {/* <hr />
                 {isSearchEnabled ? (
                   <div className={'searchWrapper navBarUL'}>
                     <Search collapse indices={searchIndices} />
                   </div>
-                  ): null}
+                ) : null} */}
               </div>
-              <ul className={'nav navbar-nav navBarUL navBarNav navbar-right navBarULRight'}>
+              {/* <ul className={'nav navbar-nav navBarUL navBarNav navbar-right navBarULRight'}>
                 {headerLinks.map((link, key) => {
                   if(link.link !== '' && link.text !== '') {
                     return(
@@ -118,7 +126,7 @@ const Header = ({location}) => (
                   (<li className={'githubBtn'}>
                     <GitHubButton href={githubUrl} data-show-count="true" aria-label="Star on GitHub">Star</GitHubButton>
                   </li>) : null}
-              </ul>
+              </ul> */}
             </div>
           </nav>
         </div>
