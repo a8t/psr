76682ce97c6c4ac05d2c837d0ae5a5cd225b8d6e
Andy Tran <andy8tran@gmail.com>
2019-09-28 19:32:39 -0400

    Clean up Headers and right sidebar links

--- src/components/mdxComponents/index.js
+++ src/components/mdxComponents/index.js
@@ -6,54 +6,12 @@ import AnchorTag from './anchor';
 
 /* eslint-disable react/display-name */
 export default {
-  h1: props => (
-    <Heading
-      id={props.children.replace(/\s+/g, '').toLowerCase()}
-      {...props}
-      is="h1"
-      fontSize={[5]}
-    />
-  ),
-  h2: props => (
-    <Heading
-      id={props.children.replace(/\s+/g, '').toLowerCase()}
-      {...props}
-      is="h2"
-      fontSize={[4]}
-    />
-  ),
-  h3: props => (
-    <Heading
-      id={props.children.replace(/\s+/g, '').toLowerCase()}
-      {...props}
-      is="h3"
-      fontSize={3}
-    />
-  ),
-  h4: props => (
-    <Heading
-      id={props.children.replace(/\s+/g, '').toLowerCase()}
-      {...props}
-      is="h4"
-      fontSize={2}
-    />
-  ),
-  h5: props => (
-    <Heading
-      id={props.children.replace(/\s+/g, '').toLowerCase()}
-      {...props}
-      is="h5"
-      fontSize={1}
-    />
-  ),
-  h6: props => (
-    <Heading
-      id={props.children.replace(/\s+/g, '').toLowerCase()}
-      {...props}
-      is="h6"
-      fontSize={0}
-    />
-  ),
+  h1: props => <Heading {...props} is="h1" fontSize={[5]} />,
+  h2: props => <Heading {...props} is="h2" fontSize={[4]} />,
+  h3: props => <Heading {...props} is="h3" fontSize={3} />,
+  h4: props => <Heading {...props} is="h4" fontSize={2} />,
+  h5: props => <Heading {...props} is="h5" fontSize={1} />,
+  h6: props => <Heading {...props} is="h6" fontSize={0} />,
   p: props => <Text {...props} is="p" lineHeight={1.625} mt={3} mb={4} />,
   pre: Pre,
   a: props => <AnchorTag {...props} />,
--- src/components/rightSidebar.js
+++ src/components/rightSidebar.js
@@ -1,132 +1,103 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 import styled from 'react-emotion';
-import config from '../../config';
 
-const Sidebar = styled('aside')`
-  width: 100%;
-  background-color: #fff;
+const RightSideBarWrapper = styled('aside')`
+  width: calc(100% - 32px);
   border-right: 1px solid #ede7f3;
   overflow: auto;
   position: fixed;
-  padding-left: 24px;
   position: -webkit-sticky;
   position: -moz-sticky;
   position: sticky;
-  top: 0;
+  top: 32px;
+
+  margin-top: 32px;
+  margin-left: 32px;
+  padding-left: 8px;
+  border-left: 1px solid #e6ecf1;
+  border-left-color: rgb(230, 236, 241);
+
   @media only screen and (max-width: 50rem) {
     width: 100%;
     position: relative;
   }
-`;
 
-// eslint-disable-next-line no-unused-vars
-const ListItem = styled(({ className, active, level, ...props }) => {
-  return (
-    <li className={className}>
-      <a href={props.to} {...props} />
-    </li>
-  );
-})`
-  list-style: none;
+  header {
+    font-size: 10px;
+    line-height: 1;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 1.2px;
+    padding: 7px 0px 7px 0px;
+    color: #9daab6;
+  }
 
-  a {
-    width: 150px;
-    color: #5c6975;
-    text-decoration: none;
-    font-weight: ${({ level }) => (level === 0 ? 700 : 400)};
-    padding: 0.45rem 0 0.45rem ${props => 2 + (props.level || 0) * 1}rem;
-    display: block;
-    position: relative;
+  ul {
+    margin: 0;
+    li {
+      list-style-type: none;
+      a {
+        font-size: 12px;
+        font-weight: 500;
+        line-height: 1.5;
+        padding: 7px 16px 7px 0px;
 
-    &:hover {
-      color: rgb(116, 76, 188) !important;
-    }
+        width: 150px;
+        color: #5c6975;
+        text-decoration: none;
+        display: block;
+        position: relative;
 
-    ${props =>
-      props.active &&
-      `
-      color: #663399;
-      border-color: rgb(230,236,241) !important;
-      border-style: solid none solid solid;
-      border-width: 1px 0px 1px 1px;
-      background-color: #fff;
-    `} // external link icon
-    svg {
-      float: right;
-      margin-right: 1rem;
+        &:hover {
+          color: rgb(116, 76, 188) !important;
+        }
+      }
     }
   }
 `;
 
-const SidebarLayout = ({ location }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allMdx {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              tableOfContents
-            }
+const query = graphql`
+  query {
+    allMdx {
+      edges {
+        node {
+          fields {
+            slug
           }
+          tableOfContents
         }
       }
-    `}
-    render={({ allMdx }) => {
-      let navItems = [];
-      let finalNavItems;
-      if (allMdx.edges !== undefined && allMdx.edges.length > 0) {
-        const navItems = allMdx.edges.map((item, index) => {
-          let innerItems;
-          if (item !== undefined) {
-            if (
-              item.node.fields.slug === location.pathname ||
-              config.gatsby.pathPrefix + item.node.fields.slug ===
-                location.pathname
-            ) {
-              if (item.node.tableOfContents.items) {
-                innerItems = item.node.tableOfContents.items.map(
-                  (innerItem, index) => {
-                    const itemId = innerItem.title
-                      ? innerItem.title.replace(/\s+/g, '').toLowerCase()
-                      : '#';
-                    return (
-                      <ListItem key={index} to={`#${itemId}`} level={1}>
-                        {innerItem.title}
-                      </ListItem>
-                    );
-                  }
-                );
-              }
-            }
-          }
-          if (innerItems) {
-            finalNavItems = innerItems;
-          }
-        });
+    }
+  }
+`;
+
+const SidebarLayout = ({ location }) => {
+  const { allMdx } = useStaticQuery(query);
+
+  const navItems = allMdx.edges
+    .filter(({ node }) => node.fields.slug === location.pathname)
+    .map(({ node }) => {
+      if (node.tableOfContents.items) {
+        return node.tableOfContents.items.map(({ title, url }) => (
+          <li key={url}>
+            <a href={url}>{title}</a>
+          </li>
+        ));
       }
 
-      if (finalNavItems && finalNavItems.length) {
-        return (
-          <Sidebar>
-            <ul className={'rightSideBarUL'}>
-              <div className={'rightSideTitle'}>CONTENTS</div>
-              {finalNavItems}
-            </ul>
-          </Sidebar>
-        );
-      } else {
-        return (
-          <Sidebar>
-            <ul></ul>
-          </Sidebar>
-        );
-      }
-    }}
-  />
-);
+      return false;
+    })
+    .filter(Boolean); // filter falses
+
+  return (
+    navItems.length > 0 && (
+      <RightSideBarWrapper>
+        <header>CONTENTS</header>
+        <ul>{navItems}</ul>
+      </RightSideBarWrapper>
+    )
+  );
+};
 
 export default SidebarLayout;
