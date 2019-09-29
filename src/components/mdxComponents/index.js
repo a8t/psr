4546f1351c434c2e90ffee09a0601abad68f3598
Andy Tran <andy8tran@gmail.com>
2019-09-28 20:33:26 -0400

    Remove Pre component

--- src/components/mdxComponents/index.js
+++ src/components/mdxComponents/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Heading from './heading';
 import Text from './text';
-import Pre from './pre';
 import AnchorTag from './anchor';
 
 /* eslint-disable react/display-name */
@@ -13,7 +12,6 @@ export default {
   h5: props => <Heading {...props} is="h5" fontSize={1} />,
   h6: props => <Heading {...props} is="h6" fontSize={0} />,
   p: props => <Text {...props} is="p" lineHeight={1.625} mt={3} mb={4} />,
-  pre: Pre,
   a: props => <AnchorTag {...props} />,
   // TODO add `img`
   // TODO add `blockquote`
