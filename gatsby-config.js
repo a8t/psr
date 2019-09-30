4c9fa04e11b935ea1fc71b19bed6e2f1a5ecca64
Andy Tran <andy8tran@gmail.com>
2019-09-30 12:54:05 -0400

    Turn on sourcemaps, turn off css purging in dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ const plugins = [
     options: {
       rejected: true,
       printRejected: true, // Print removed selectors and processed file names
-      develop: true, // Enable while using `gatsby develop`
+      develop: false, // Enable while using `gatsby develop`
       // tailwind: true, // Enable tailwindcss support
       // whitelist: ['whitelist'], // Don't remove this selector
       // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
